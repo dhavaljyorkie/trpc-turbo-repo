4851298db6d5c504a1b9c5aaad51bb5fd7038cc9
Dhaval <dhavalj@york.ie>
2025-05-23 14:27:24 +0530

    feat: greetings added

---
+++ apps/admin/src/app/welcome/index.tsx
@@ -0,0 +1,17 @@
+export function Greeting() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "1rem",
+        padding: "1rem",
+        border: "1px solid #ccc",
+        borderRadius: "8px",
+      }}
+    >
+      <h2>Greeting</h2>
+      <button onClick={() => {}}>Create user</button>
+    </div>
+  );
+}
